32d8098fd948903ee19bf9449317e09ad0caa193
riffelj <jriffel@xpanxion.com>
2023-07-21 10:01:26 -0500

    replace hardcoded product category path

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Products() {
     const [size, setSize] = useState("");
+    const { category } = useParams();
 
     const { data: products, loading, error } = useFetch(
-        "products?category=shoes"
+        `products?category=${category}`
     );
 
     function renderProduct(p) {
