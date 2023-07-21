869ae1448b7166db653456b1f5ebb79c46e76943
riffelj <jriffel@xpanxion.com>
2023-07-21 11:11:07 -0500

    Use NavLink to navigate to app pages

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+// activeStyle is used in the NavLink tag to style the currently active route
+const activeStyle = { color: "purple", fontWeight: "bold" };
 
 export default function Header() {
   return (
@@ -6,7 +10,15 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <NavLink to="/">
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shoes" activeStyle={activeStyle}>Shoes</NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" activeStyle={activeStyle}>Cart</NavLink>
           </li>
         </ul>
       </nav>
