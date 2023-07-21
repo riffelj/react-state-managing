bd0917f3dd0c9d1fdc9a627a0ad19d821f6c32e7
riffelj <jriffel@xpanxion.com>
2023-07-21 10:48:32 -0500

    Use Link to navigate to a product detail page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const { data: product, loading, error } = useFetch(`products/${id}`);
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  if (loading) return <Spinner />;
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
